7623c0a6c72aa7be21136b74f7d51e759437c59e
Barakat Akinrinde <barakatakinrinde@gmail.com>
2024-04-26 11:51:42 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] words = {"aja", "dad", "noon", "figure", "immense"};
+        int count = CountPalindromes(words);
+        Console.WriteLine($"No of Palindrome is {count}");
+    }
+
+    static int CountPalindromes(string[] words)
+    {
+      int count = 0;
+      foreach (string word in words)
+      {
+        if (IsPalindrome(word))
+        {
+          count ++;
+        }
+      }
+      return count;
+    }
+
+    static bool IsPalindrome(string word)
+    {
+      return word.SequenceEqual(word.Reverse());
+    }
+}
